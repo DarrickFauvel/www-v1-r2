1207582089982639cd07159b9d9e3b763a10c70a
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-06-19 12:33:20 -0400

    Create Footer component file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='container footer-container'>
+        <div className='footer-content'>
+          Made with{' '}
+          <span role='img' aria-label='heart emoji'>
+            ❤️
+          </span>{' '}
+          in Massachusetts.
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
