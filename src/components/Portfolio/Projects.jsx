af0b64af77552aa26af7c89d81d194da632b1315
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-06-28 10:40:20 -0400

    Create Projects component

---
+++ src/components/Portfolio/Projects.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Card from './Card'
+import Modal from './Modal'
+
+const Projects = ({ projects, activeTab, tabs }) => {
+  const [selectedProject, setSelectedProject] = useState(null)
+  const [showModal, setShowModal] = useState(false)
+
+  const openModal = (project) => {
+    setSelectedProject(project)
+    setShowModal(true)
+    console.log(selectedProject, showModal)
+  }
+
+  const closeModal = () => {
+    setSelectedProject(null)
+    setShowModal(false)
+    console.log(selectedProject, showModal)
+  }
+
+  const cardsTitle = tabs.find((tab) => {
+    return tab.title.toLowerCase() === activeTab.toLowerCase()
+  })
+
+  return (
+    <>
+      <h3 className='tab-content--title'>{cardsTitle.subtitle}</h3>
+      <div className='cards'>
+        {projects.map((project, index) => {
+          return <Card project={project} key={index} openModal={openModal} />
+        })}
+      </div>
+      {/* END cards */}
+      {showModal && <Modal project={selectedProject} closeModal={closeModal} />}
+    </>
+  )
+}
+
+export default Projects
