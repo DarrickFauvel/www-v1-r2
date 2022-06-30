00d2bcb219438907146c77f853b6e298c5ac0df8
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-06-29 21:27:55 -0400

    Rename class name

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <section id='contact'>
       <div className='container contact-container'>
         <div className='contact-head'>
-          <ChatAlt2Icon className='contact-head--icon' />
+          <ChatAlt2Icon className='section-head--icon' />
           <h1 className='contact-head--title'>Contact Me</h1>
           <p className='contact-head--text'>
             Let's see if we're a match and work together.
