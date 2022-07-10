7d400f45b087e84546e5aad240e7ccaffc183ea5
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-07-09 21:32:45 -0400

    Create home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import About from '../components/About'
+import Portfolio from '../components/Portfolio'
+import Skills from '../components/Skills'
+import Contact from '../components/Contact'
+
+const Home = () => {
+  return (
+    <>
+      <About />
+      <Portfolio />
+      <Skills />
+      <Contact />
+    </>
+  )
+}
+
+export default Home
