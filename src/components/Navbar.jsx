5dbbea512a3f7e234b41afd5e2758c7b35ddc2ce
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-07-09 21:31:59 -0400

    Add motion to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,69 @@
+import { Link } from 'react-router-dom'
+import { motion, AnimatePresence } from 'framer-motion'
 import { ArrowDownIcon } from '@heroicons/react/solid'
+
+const brandVariants = {
+  hidden: {
+    opacity: 0,
+    x: -30
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.3,
+      duration: 0.5
+    }
+  }
+}
+
+const navVariants = {
+  hidden: {
+    opacity: 0,
+    x: 30
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.3,
+      duration: 0.5
+    }
+  }
+}
+
+const buttonVariants = {
+  hidden: {
+    opacity: 0,
+    y: -30
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1,
+      duration: 0.5
+    }
+  }
+}
 
 const Navbar = () => {
   return (
     <div className='container navbar-container'>
-      <div className='navbar-brand'>
+      <motion.div
+        className='navbar-brand'
+        variants={brandVariants}
+        initial='hidden'
+        animate='visible'>
         <a href='#about' className='navbar-brand__link'>
           Darrick Fauvel
         </a>
-      </div>
-      <nav className='nav'>
+      </motion.div>
+      <motion.nav
+        className='nav'
+        variants={navVariants}
+        initial='hidden'
+        animate='visible'>
         <a href='#portfolio' className='nav__link'>
           Portfolio
         </a>
@@ -19,17 +74,22 @@ const Navbar = () => {
           Contact
         </a>
         {/* <a href='#testimonials' className='nav__link'>
-            Testimonials
-          </a> */}
-      </nav>
-      <a href='#contact' className='nav-contact__link'>
+              Testimonials
+            </a> */}
+      </motion.nav>
+      <motion.a
+        href='#contact'
+        className='nav-contact__link'
+        variants={buttonVariants}
+        initial='hidden'
+        animate='visible'>
         <div className='nav-contact__link--text'>
           <div>Hire Me</div>
           <div>
             <ArrowDownIcon className='nav-arrow' />
           </div>
         </div>
-      </a>
+      </motion.a>
     </div>
   )
 }
