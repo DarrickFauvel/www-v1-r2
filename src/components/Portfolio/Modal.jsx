eddc2cd2874144322da3f52b616bb771b8be7fae
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-07-09 21:31:28 -0400

    Update modal component

--- src/components/Portfolio/Modal.jsx
+++ src/components/Portfolio/Modal.jsx
@@ -1,64 +1,102 @@
+import ReactDom from 'react-dom'
 import TechStack from './TechStack'
 
-const Modal = ({ project, closeModal }) => {
-  return (
-    <div className='modal' tabIndex='-1' onClick={closeModal}>
-      <div className='modal-dialog' onClick={(e) => e.stopPropagation()}>
-        <div className='modal-content'>
-          <div className='modal-header'>
-            <h5 className='modal-title'>{project.title}</h5>
-            <div className='modal-image'>
-              <img src={project.image} alt='' />
-            </div>
-          </div>
-          {/* END modal-header */}
+const OVERLAY_STYLES = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  zIndex: 1000
+}
 
-          <div className='modal-scroll-area'>
-            <div className='modal-body'>
-              <p>{project.description}</p>
-            </div>
-            <TechStack project={project} />
-            <ul className='modal-links'>
-              <li className='links-item'>
-                <a
-                  href={project.liveUrl}
-                  target='_blank'
-                  className='links-link'
-                  title='Go to live site on Netlify'>
-                  <img src='./netlify-logo-cropped.png' alt='' />
-                  <span>Live Site</span>
-                </a>
-              </li>
-              <li className='links-item'>
-                <a
-                  href={project.repoUrl}
-                  className='links-link'
-                  target='_blank'>
-                  <i className='fa-brands fa-github fa-2xl'></i>
-                  <span>GitHub</span>
-                </a>
-              </li>
-            </ul>
-            {/* END modal-links */}
-          </div>
-          {/* END modal-scroll-area */}
+const MODAL_STYLES = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#FFF',
+  padding: '50px',
+  zIndex: 1000
+}
 
-          <div className='modal-footer'>
-            <button
-              type='button'
-              className='btn btn-secondary'
-              data-bs-dismiss='modal'
-              onClick={closeModal}>
-              Close
-            </button>
-          </div>
-          {/* END modal-footer */}
+const Modal = ({ children, project, isModalOpen, closeModal }) => {
+  if (!isModalOpen) return null
+
+  return ReactDom.createPortal(
+    <>
+      <div className='modal' onClick={closeModal} />
+
+      <div className='modal-dialog'>
+        <div className='modal-header'>
+          <h4 className='modal-title'>{project.titleLong}</h4>
         </div>
-        {/* END modal-content */}
+        <div className='modal-image'>
+          <img src={project.image} alt='' />
+        </div>
+        <div className='modal-body'>
+          <p>
+            <span>Client</span>
+            {project.client}
+          </p>
+          <p>
+            <span>Goal</span>
+            {project.description.goal}
+          </p>
+          {project.description.inspiration && (
+            <p>
+              <span>Inspiration</span>
+              {project.description.inspiration}
+            </p>
+          )}
+          {project.description.challenge && (
+            <p>
+              <span>Challenge</span>
+              {project.description.challenge}
+            </p>
+          )}
+          {project.description.solution && (
+            <p>
+              <span>Solution</span>
+              {project.description.solution}
+            </p>
+          )}
+          {project.description.future && (
+            <p>
+              <span>Future Feature</span>
+              {project.description.future}
+            </p>
+          )}
+          <TechStack project={project} />
+          <ul className='modal-links'>
+            <li>
+              <a className='links-link' href={project.liveUrl} target='_blank'>
+                <img src='/netlify-logo-cropped.png' alt='' />
+                <span>Demo</span>
+              </a>
+            </li>
+            <li>
+              <a className='links-link' href={project.repoUrl} target='_blank'>
+                <img
+                  className='github-logo'
+                  src='/github-icon-logo-png-transparent.png'
+                  alt=''
+                />
+                <span>Code</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* END modal-body */}
+
+        <button className='btn' onClick={closeModal}>
+          Close
+        </button>
       </div>
       {/* END modal-dialog */}
-    </div>
-    // END modal
+    </>,
+    document.getElementById('portal')
   )
 }
 
